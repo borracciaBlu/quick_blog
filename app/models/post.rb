3cdd7f7cff5d71a40ce9c481e9a10cf731f0baff
borracciaBlu <marco.asdman@gmail.com>
2013-12-05 21:04:29 +1100

    initial blog commit

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ActiveRecord::Base
+	# attr_accessible :body, :title
+
+  	has_many :comments
+
+	validates_presence_of :body, :title
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QuickBlog::Application.config.session_store :cookie_store, key: '_quick_blog_session'
