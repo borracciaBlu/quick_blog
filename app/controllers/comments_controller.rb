6786dea67ff052259ac2018413874faafd053460
borracciaBlu <marco.asdman@gmail.com>
2013-12-10 20:24:31 +1100

    fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(params[:comment_body])
-    puts YAML::dump(params)
+    @comment = @post.comments.create!(comment_params)
+    # @comment = @post.comments.create!(params[:comment_body])
     redirect_to @post
   end
+
+  private
+	  def comment_params
+	    params.require(:comment).permit(:body)
+	  end
 end
